4cc4cb2211f96368694457c0fd7cb5e91b38aa2f
Steven Boyd-Thompson <steveboydt@gmail.com>
2019-04-01 21:28:53 +0100

    Require user confirmation if file exists

--- src/path-processor.ts
+++ src/path-processor.ts
@@ -6,48 +6,75 @@ import * as nameProvider from './providers/name-provider';
 import * as templateHelpers from './helpers/file-template-helpers';
 
 export function processPath(
-    path: string | undefined,
-    context: vscode.ExtensionContext
-  ): void {
-    if (path) {
-      const customConfigPath = fileConfigurationProvider.getCustomConfigFilePath(
-        context
-      );
-      const normalizedResult = stringHelpers.replaceAll(path, '/', '\\');
-      const resultParts = stringHelpers.split(normalizedResult, '\\', true);
-  
-      if (!resultParts) {
-        return;
-      }
-  
-      let builtPath: string = '';
-      for (let pathPart of resultParts) {
-        builtPath = builtPath.concat(pathPart);
-        const fileConfiguration = fileConfigurationProvider.getFileConfiguration(
-          pathPart,
-          customConfigPath.fsPath
-        );
-  
-        if (fileConfiguration.Identifier === 'Directory') {
-          fileSystem.createDirectory(builtPath);
-          continue;
-        }
-        let template = fileConfiguration.FileTemplate;
-  
-        var runReplacement = templateHelpers.hasReplaceableSections(template);
-  
-        if (runReplacement) {
-          var fileNameReplacement = nameProvider.getNameFromFileName(pathPart);
-          template = templateHelpers.runReplacement(
-            template,
-            fileNameReplacement
-          );
-        }
-  
-        fileSystem.writeToFile(builtPath, template);
-        vscode.workspace
-          .openTextDocument(builtPath)
-          .then(doc => vscode.window.showTextDocument(doc));
-      }
-    }
+  path: string | undefined,
+  context: vscode.ExtensionContext
+): void {
+  if (!path) {
+    return;
   }
+
+  const normalizedResult = normalizePath(path);
+  const resultParts = splitPath(normalizedResult);
+
+  if (!resultParts) {
+    return;
+  }
+
+  if (fileSystem.fileExists(path)) {
+    const userOptions = ['Continue', 'Cancel'];
+    vscode.window
+      .showInformationMessage('File already exists.', ...userOptions)
+      .then((selection: string | undefined) => {
+        if (!selection || selection === userOptions[1]) {
+          return;
+        } else {
+          processSplitPath(resultParts, context);
+        }
+      });
+  } else {
+    processSplitPath(resultParts, context);
+  }
+}
+
+function normalizePath(path: string): string {
+  return stringHelpers.replaceAll(path, '/', '\\');
+}
+
+function splitPath(path: string): string[] | null {
+  return stringHelpers.split(path, '\\', true);
+}
+
+function processSplitPath(
+  pathParts: string[],
+  context: vscode.ExtensionContext
+): void {
+  const customConfigPath = fileConfigurationProvider.getCustomConfigFilePath(
+    context
+  );
+  let builtPath: string = '';
+  for (let pathPart of pathParts) {
+    builtPath = builtPath.concat(pathPart);
+    const fileConfiguration = fileConfigurationProvider.getFileConfiguration(
+      pathPart,
+      customConfigPath.fsPath
+    );
+
+    if (fileConfiguration.Identifier === 'Directory') {
+      fileSystem.createDirectory(builtPath);
+      continue;
+    }
+    let template = fileConfiguration.FileTemplate;
+
+    var runReplacement = templateHelpers.hasReplaceableSections(template);
+
+    if (runReplacement) {
+      var fileNameReplacement = nameProvider.getNameFromFileName(pathPart);
+      template = templateHelpers.runReplacement(template, fileNameReplacement);
+    }
+
+    fileSystem.writeToFile(builtPath, template);
+    vscode.workspace
+      .openTextDocument(builtPath)
+      .then(doc => vscode.window.showTextDocument(doc));
+  }
+}
